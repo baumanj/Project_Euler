4c868278450b0c7010e1954a978e74a5b37ba707
Jon Bauman <jongithub@shumi.fake>
2013-11-21 00:59:08 -0800

    Forgot about the even? method. A nice one for readability.

--- Problem2.rb
+++ Problem2.rb
@@ -9,8 +9,8 @@ y = 2 # The problem states the sequence starts with 1, 2
 # Finally, using x as your while condition allows for a simpler body
 sum = 0
 while x <= 4_000_000  # The problem says, "do not exceed", not less than
-  sum += x if x % 2 == 0 # Ruby supports postfix conditionals
-  x, y = y, x + y        # and parallel assignment
+  sum += x if x.even? # Ruby supports postfix conditionals
+  x, y = y, x + y     # and parallel assignment
 end
 
 puts sum
