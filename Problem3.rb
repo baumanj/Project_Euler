17cf5541c0b9d241f7c2c1750053e230ff650f80
Jon Bauman <jongithub@shumi.fake>
2013-11-21 01:04:37 -0800

    Most of the changes here are just awareness of useful ruby library methods like none? and reduce. Also, the ternary expression <condition> ? <if-true> : <else> is useful in a lot of contexts.

--- Problem3.rb
+++ Problem3.rb
@@ -3,16 +3,22 @@
 
 n = 600851475143
 # Returns array of factors of n
-range = (2..Math.sqrt(n))
-factors = range.select {|factor| n % factor == 0}
+factors = (2..Math.sqrt(n)).select {|factor| n % factor == 0}
 # [71, 839, 1471, 6857, 59569, 104441, 486847] 
 
 # Returns array "factors" containing only the prime numbers from the array "factors"
-factors.keep_if do |factor|
-  (2..Math.sqrt(factor)).select {|x| factor % x == 0} == []
+
+# This function is unnecessary since we have Prime.prime?, but I wanted 
+# to show you how I'd rewrite your prime testing algorithm
+def prime?(n)
+  (2..Math.sqrt(n)).none? {|x| n % x == 0}
 end
 
-# Finds max of prime factors of n
+# Almost any time you're generating a single number from an Enumerable,
+# reduce (aka inject) works nicely
+max_factor = factors.reduce do |max_so_far, next_factor|
+  prime?(next_factor) ? next_factor : max_so_far
+end
 
-factors.max
+puts max_factor
 
